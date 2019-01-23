dc9e1ee674c984dace2046ba6e0180556c3f5173
junderw <junderwood@bitcoinbank.co.jp>
2019-01-23 16:43:18 +0900

    Fix BIP32 path bug and support more than 3 depth paths

--- src/bip32_path.js
+++ src/bip32_path.js
@@ -19,14 +19,16 @@ const hardenedMarker = "'"
   <BIP 32 Path Buffer Object>
 */
 module.exports = ({ path }) => {
-  const [, child, childHardened, childIndex] = path.split(bip32PathSeparator)
+  const indices = path.split(bip32PathSeparator)
 
-  return concat([child, childHardened, childIndex].map(n => {
+  return concat(indices.slice(1).map(n => {
     const len = hardenedMarker.length
 
-    const path = n.slice(-len) === hardenedMarker ? n.slice(0, -len) : n
+    const isHardened = n.slice(-len) === hardenedMarker
 
-    const value = parseInt(path, decBase) + bip32KeyLimit
+    const path = isHardened ? n.slice(0, -len) : n
+
+    const value = parseInt(path, decBase) + (isHardened ? bip32KeyLimit : 0)
 
     return new BN(value, decBase).toArrayLike(Buffer, endianness, byteLength)
   }))
